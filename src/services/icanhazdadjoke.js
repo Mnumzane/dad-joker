d16b39d929a8755eba0f4886d29edb9e6d5e04aa
Bongi Fleischer <bongifleischer@berkeley.edu>
2021-09-13 22:53:18 -0700

    Random joke fetching API working.

--- src/services/icanhazdadjoke.js
+++ src/services/icanhazdadjoke.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://api.coingecko.com/api/v3'
+const API_BASE_URL = 'https://icanhazdadjoke.com'
 export const getRandomJoke = async () => {
     return await axios({
+        headers:
+            { "Accept": "application/json" },
         method: 'GET',
-        url: `${API_BASE_URL}`
+        url: `${API_BASE_URL}/`
     })
 }
